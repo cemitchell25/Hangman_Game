b9ead9b9824e3ffd92d51148dda64d36f05ca32b
Kelsey Elmer <kguitar6@Ks-MacBook-Air.local>
2017-05-18 17:03:45 -0600

    new additions

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 
 
+
       var food = [
         "waffles",
         "tamales",
@@ -10,68 +11,51 @@
         ];
 
 
-        var randomWord = food[Math.floor(Math.random() * food.length)];
-        var s;
         var count =0;
         var answerArray = [];
 
       function gameTime() {
 
+         var randomWord = food[Math.floor(Math.random() * food.length)];
+
+
         for (var i=0; i<randomWord.length; i++) {
           answerArray[i] = "_";
         }
 
-    
-        s = answerArray.join(" ");
-        document.getElementById("answer").innerHTML = s;
-
-          }
-
-        function guess() {
-          var guess = document.getElementById("guess").value;
-
-          if (guess.length > 0) {
-
-            for (var i=0; i<randomWord.length; i++)
-              {
-                if (randomWord[i] === guess)
-                { answerArray[i] == guess;
-
-
-                }}}
-
-        };
-
-        count++;
         document.getElementById("answer").innerHTML = answerArray.join(" ");
+          
+
+
+
+                function playerGuess() {
+
+                var letter = document.getElementById("letter").value;
+
+                if (letter.length > 0)
+
+                    {   for (var i = 0; randomWord.length > i; i++)
+
+                        { if (randomWord[i] === letter)
+
+                            {
+                                answerArray[i] = letter;
+                            }
+
+                        }
+                
+                count++;
+                document.getElementById("guesses").innerHTML = "# of guesses: " + count;
+                document.getElementById("answer").innerHTML = answerArray.join(" ");
+
+                    }}}
+
+
 
       
 
-
-
         
-        // var guess = prompt("Guess a letter!");
-
-        //   if (guess === null) {
-        //     break;
-        //   }
-        //   else if (guess.length !== 1) {
-        //     alert("Please enter a single letter");
-        //   }
-
-        //   else {
-        //     for(var j=0; j<word.length; j++) {
-        //       if (word[j] === guess) {
-        //         answerArray[j] = guess;
-        //         remainingLetters--;
-        //       }
-        //     }
-        //   }
-        // }
         
+    
 
 
-        //   document.getElementById(answerArray.join(" "));
-        //   alert("Good job! The answer was " + word);
-
-        // };
