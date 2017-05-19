176182e146247ddc90b82f8c2fb7e646fbb63081
Austin <elektricwebdesign@gmail.com>
2017-05-18 19:51:30 -0600

    just a couple thoughts :):

--- javascript.js
+++ javascript.js
@@ -13,68 +13,54 @@
         var randomWord = food[Math.floor(Math.random() * food.length)];
         var count =0;
         var answerArray = [];
-
+        /**
+        @var answerTargetEl this will be our p tag we inject our dashes and update our words El is short for element btw :)
+        */
+        var answerTargetEl;
       function gameTime() {
 
-
+        console.log(' IM THE RANDOM WORD!', randomWord);
                 for (var i=0; i<randomWord.length; i++) {
-                answerArray[i] = "_";
+                answerArray.push("_");
                 }
 
-        document.getElementById("answer").innerHTML = answerArray.join(" ");
+        answerTargetEl = document.getElementById("answer").innerHTML = answerArray.join(" ");
 
         };
-          
 
 
 
-        function playerGuess() {
 
-            var letter = document.getElementById("letter").value;
+        function playerGuess(letter) {
+          console.log(' PLAYER GUESS FIRE!');
 
-                if (letter.length > 0)
-
-                    {   for (var i = 0; randomWord.length > i; i++)
-
-                        { if (randomWord[i] === letter)
-
-                            {
+                  for (var i = 0; randomWord.length > i; i++) {
+                      if (randomWord.charAt(i) === letter) {
+                        // console.log('IM THE answerArray[i]', randomWord.charAt(i), letter );
                                 answerArray[i] = letter;
+                                // answerTargetEl.innerHTML  = answerArray.join("");
                             }
 
-                            console.log(answerArray);
-                            console.log(letter.length);
+                            // console.log(letter.length);
                             console.log(randomWord);
                             console.log(letter);
-
-                            if (answerArray === randomWord) {
+                            let answer = answerArray.join('');
+                            console.log("Answer Array", answer);
+                            if (answer === randomWord) {
                             alert("YOU WON!");
 
                             }
 
                     }
 
-                
+
                 count++;
                 document.getElementById("guesses").innerHTML = "# of guesses: " + count;
                 document.getElementById("answer").innerHTML = answerArray.join(" ");
 
 
-                    }}
-
-  
-
-
-          
-           
-
-
-
-
-      
-
-        
-        
-    
-
-
+                    }
+                    
+          document.addEventListener('keyup', function(event) {
+              playerGuess(event.key);
+          })
