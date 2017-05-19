28d73e256e273199b0d976828735bbfc958545b0
Kelsey Elmer <kguitar6@Ks-MacBook-Air.local>
2017-05-19 15:34:53 -0600

    added new alert

--- javascript.js
+++ javascript.js
@@ -18,7 +18,7 @@
 
 
                 for (var i=0; i<randomWord.length; i++) {
-                answerArray[i] = "_";
+                    answerArray[i] = "_";
                 }
 
         document.getElementById("answer").innerHTML = answerArray.join(" ");
@@ -30,39 +30,45 @@
 
         function playerGuess() {
 
-            var letter = document.getElementById("letter").value;
+            var guess = document.getElementById("guess").value;
 
-                if (letter.length > 0)
+                if (guess.length > 0)
 
-                    {   for (var i = 0; randomWord.length > i; i++)
+                      for (var i = 0; randomWord.length > i; i++) {
 
-                        { if (randomWord[i] === letter)
+
+                         if (randomWord[i] === guess)
 
                             {
-                                answerArray[i] = letter;
+                                answerArray[i] = guess;
                             }
 
-                            console.log(answerArray);
-                            console.log(letter.length);
-                            console.log(randomWord);
-                            console.log(letter);
 
-                            if (answerArray === randomWord) {
-                            alert("YOU WON!");
+                           
+                            if (randomWord === answerArray.join("")) 
+                            { alert("YOU WON! The correct word was " + randomWord + "!");
 
                             }
 
                     }
 
+                         console.log(food[i]);
+                            console.log(answerArray.join(""));
+                            console.log(randomWord);
+                            console.log(guess);
+
                 
                 count++;
                 document.getElementById("guesses").innerHTML = "# of guesses: " + count;
                 document.getElementById("answer").innerHTML = answerArray.join(" ");
+                // document.getElementById("catwin").innerHTML = (img src= "../Hangman_Game/pusheen.jpg");
 
 
-                    }}
+                 }    
+                
 
   
+     
 
 
           
